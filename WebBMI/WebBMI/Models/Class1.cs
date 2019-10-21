7101701e853a4efa7107ce57c17b6067d6d9ee9f
unknown <f22325076@gmail.com>
2019-10-21 09:52:12 +0800

    Add LifeNum

---
+++ WebBMI/WebBMI/Models/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBMI.Models
+{
+ 
+        public class BMIResult
+        {
+            public float BMI { get; set; }
+        }
+
+        public class BMIParam
+        {
+            public float height { get; set; }
+            public float weight { get; set; }
+        }
+
+}
